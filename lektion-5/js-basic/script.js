99a9ec6b8917df26cdc09149e4401098ecea8c6c
Hans Mattin-Lassei <hans.mattin-lassei@lexicon.se>
2021-05-24 10:07:46 +0200

    Föreläsning 5

---
+++ lektion-5/js-basic/script.js
@@ -0,0 +1,21 @@
+/* 
+    Chromium, Chrome, Edge (ny), Opera      =   V8
+    FireFox                                 =   SpiderMonkey
+    IE, Edge (gamla)                        =   Chakra
+    Safari                                  =   JavascriptCore/SquirrelFish
+
+    Javascriptsversioner                    =   ES/EcmaScript
+    Javascript                              !=  Java
+
+    Javascript, powershell, (python)        =   scriptspråk
+    C/C++/C#, Java, (python)                =   programmeringsspråk         (skapar -> .exe)
+
+    Script                      Program
+    ------------------------------------------------
+    Javascript  .js     ->      chrome.exe, edge.exe
+    PowerShell  .ps1    ->      powershell.exe
+    Python      .py     ->      pyhton.exe/node.exe
+    Word        .docx   ->      word.exe
+
+
+*/
